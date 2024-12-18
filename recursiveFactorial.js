fea45130eaa89f181b73b31bc4724d6e83309342
Shadan9113 <mdshadanzaheer@gmail.com>
2024-12-18 14:01:37 +0530

    spelling mistake

--- recursiveFactorial.js
+++ recursiveFactorial.js
@@ -3,6 +3,6 @@ const recursiveFactorial = (n) => {
         if(n === 0){
             return 1;
         }
-        return  n * reacursiveFactorial(n-1)
+        return  n * recursiveFactorial(n-1)
 }
 console.log(recursiveFactorial(5));
